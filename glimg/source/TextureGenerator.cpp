89f151b9f170bbb9096c7ee05ab804b06236c625
korval2 <https://sourceforge.net/u/korval2/profile>
2011-05-17 13:11:04 -0700

    glimg: Fixed an exception safety failure.

--- glimg/source/TextureGenerator.cpp
+++ glimg/source/TextureGenerator.cpp
@@ -1119,7 +1119,15 @@ namespace glimg
 		GLuint textureName = 0;
 		glGenTextures(1, &textureName);
 
-		CreateTexture(textureName, pImage, forceConvertBits);
+		try
+		{
+			CreateTexture(textureName, pImage, forceConvertBits);
+		}
+		catch(...)
+		{
+			glDeleteTextures(1, &textureName);
+			throw;
+		}
 
 		return textureName;
 	}
