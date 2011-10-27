c5237e38d566be36ec70792ef192127f0f536240
korval2 <https://sourceforge.net/u/korval2/profile>
2011-10-27 15:00:32 -0700

    Updated distro to remove glmesh directories.

--- make_distro.lua
+++ make_distro.lua
@@ -92,9 +92,11 @@ local toDelete =
 	--files
 	"make_distro.lua", "get_externals.lua", ".hgignore", ".hgtags",
 	"_FindFileInPath.lua", "make_copyright.lua",
-	".hg_archival.txt", "glimg/premake4.lua", "glload/premake4.lua",
+	".hg_archival.txt", "glimg/premake4.lua",
+	"glload/premake4.lua", "glmesh/premake4.lua",
 	--directories
 	"glimg/Test", "glload/Test", "glutil/Test",
+	"glmesh/Test",
 	"glm/doc/build",
 	"caps",
 }
