c5240ec13429ddf5b6b10aced5f85845bd7418b5
korval2 <https://sourceforge.net/u/korval2/profile>
2012-12-06 18:40:55 -0800

    Start of Boost extraction code.

---
+++ extract_boost.lua
@@ -0,0 +1,37 @@
+
+
+local pull =
+{
+	"any.hpp",
+	"array.hpp",
+	"assign.hpp",
+	"bind.hpp",
+	"men_fn.hpp",
+	"boost\container",
+	"foreach.hpp",
+	"format.hpp",
+	"function.hpp",
+	"lexical_cast.hpp",
+	"optional.hpp",
+	"random.hpp",
+
+	"range.hpp",
+	"boost\range\adaptors.hpp",
+	"boost\range\algorithm.hpp",
+	"scoped_ptr.hpp",
+	"smart_ptr.hpp",
+	"weak_ptr.hpp",
+	"static_assert.hpp",
+	"algorithm\string.hpp",
+
+	"tuple\tuple.hpp",
+	"tuple\tuple_comparison.hpp",
+	"type_traits.hpp",
+	"unordered_set.hpp",
+	"unordered_map.hpp",
+	"utility.hpp",
+	"variant.hpp",
+	"boost\variant",
+	"boost\algorithm\cxx11",
+}
+
