04e1f1b7e5d9b2111fbcc46d65a5e33328002104
korval2 <https://sourceforge.net/u/korval2/profile>
2012-12-08 18:09:57 -0800

    Allow libraries to be built in a separate solution.

--- links.lua
+++ links.lua
@@ -73,17 +73,53 @@ local LinkFuncs =
 	end,
 }
 
-local function ProcTable(tbl)
+local InclFuncs =
+{
+	glload = function()
+		includedirs "glload/include"
+		libdirs "glload/lib"
+	end,
+	glimage = function()
+		includedirs "glimg/include"
+		libdirs "glimg/lib"
+	end,
+	glutil = function()
+		includedirs "glutil/include"
+		libdirs "glutil/lib"
+	end,
+	glmesh = function()
+		includedirs "glmesh/include"
+		libdirs "glmesh/lib"
+	end,
+	glm = function()
+		includedirs "glm"
+	end,
+	freeglut = function()
+		includedirs "freeglut/include"
+		libdirs "freeglut/lib"
+		defines {"FREEGLUT_STATIC", "_LIB", "FREEGLUT_LIB_PRAGMAS=0"}
+	end,
+	glfw = function()
+		includedirs "glfw/include"
+		libdirs "glfw/library"
+	end,
+	boost = function()
+		includedirs "boost"
+		defines {"BOOST_ALL_NO_LIB"}
+	end,
+}
+
+local function ProcTable(tbl, libTbl)
 	for i, lib in ipairs(tbl) do
 		if(type(lib) == "string") then
-			if(not LinkFuncs[lib]) then
+			if(not libTbl[lib]) then
 				print("Bad library named ", lib);
 			else
 				local prevDir = os.getcwd()
 				os.chdir(thisDirectory)
 				configuration {}
 
-				LinkFuncs[lib]()
+				libTbl[lib]()
 				
 				configuration {}
 				os.chdir(prevDir)
@@ -96,5 +132,10 @@ end
 
 function UseLibs(...)
 	local libList = {...}
-	ProcTable(libList)
+	ProcTable(libList, LinkFuncs)
+end
+
+function InclLibs(...)
+	local libList = {...}
+	ProcInclTable(libList)
 end
--- premake4.lua
+++ premake4.lua
@@ -2,21 +2,4 @@ solution "glsdk"
 	configurations {"Debug", "Release"}
 	defines(dofile("common_defines.lua"))
 
-
-local libPremakes = 
-{
-	"glload/glload.lua",
-	"glimg/glimg.lua",
-	"glfw/glfw.lua",
-	"freeglut/freeglut.lua",
-	"glutil/glutil.lua",
-	"glmesh/glmesh.lua",
-}
-
-for i, luaPremake in ipairs(libPremakes) do
-	if(#os.matchfiles(luaPremake) ~= 0) then
-		dofile(luaPremake)
-	end
-end
-	
-
+dofile("libraries.lua")
